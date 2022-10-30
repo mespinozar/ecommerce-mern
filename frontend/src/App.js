ddf336ac1ea7a1954dc0af95044163fcfc1f0547
mespinozar <rieraes@gmail.com>
2022-10-29 23:20:19 -0500

    Ecommerce mern 01

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Pedidos Ya</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
